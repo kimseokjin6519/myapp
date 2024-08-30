f420711e2d2b38e689510973165d3da465dd4eb9
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-08-30 13:04:55 +0000

    home page

--- src/App.js
+++ src/App.js
@@ -5,13 +5,8 @@ import About from './pages/About';
 
 function App() {
   return (
+
     <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/about">About</Link></li>
-        </ul>
-      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
