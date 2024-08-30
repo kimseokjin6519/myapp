0ddc8fd4364fccda755c5f161139334655ea286f
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-08-30 12:59:23 +0000

    init

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+
+function App() {
+  return (
+    <Router>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/about">About</Link></li>
+        </ul>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        {/* You can add more routes here */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
