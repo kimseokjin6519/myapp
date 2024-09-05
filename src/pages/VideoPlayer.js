c971faad6df3292b1682d46eab999e5cd6e60b26
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-09-05 09:24:53 +0000

    player-iframe

--- src/pages/VideoPlayer.js
+++ src/pages/VideoPlayer.js
@@ -1,12 +1,15 @@
 import React from 'react';
-import './VideoPlayer.css'; // Import the CSS file
+import { useParams } from 'react-router-dom'; // Import useParams hook
+import './VideoPlayer.css'; // Import the CSS file for styling
 
 function VideoPlayer() {
+  const { videoID } = useParams(); // Extract videoID from URL
+
   return (
-    <div className="video-container">
+    <div className="video-container"> {/* Ensure this is the only top-level element */}
       <iframe
         title="YouTube Video"
-        src="https://www.youtube.com/embed/dQw4w9WgXcQ" /* Example YouTube video URL */
+        src={`https://www.youtube.com/embed/${videoID}?modestbranding=1?showinfo=0?rel=0`} // Embed the video using the videoID
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
         allowFullScreen
         className="youtube-iframe"
