71c0ada30f0d62758fff767c89711db6734126e4
Kim Seok Jin <kimseokjin6519@gmail.com>
2024-09-06 10:27:25 +0000

    front end

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import About from './pages/About';
 import Signin from './pages/Signin';
 import Profile from './pages/Profile';
 import VideoPlayer from './pages/VideoPlayer';
@@ -12,7 +11,6 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
 	<Route path="/signin" element={<Signin />} />
         <Route path="/profile" element={<Profile />} />
          <Route path="/video/:videoID" element={<VideoPlayer />} />
